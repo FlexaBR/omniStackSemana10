06120710b8779928e319677c6883ed4abaec8756
FlexaBR <r.gutheil@terra.com.br>
2020-01-20 15:40:14 -0300

    first

---
+++ backend/src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+
+const routes = require('./routes');
+const { setupWebsocket } = require('./websocket')
+
+const app = express();
+const server = http.Server(app);
+
+mongoose.set('useCreateIndex', true);
+
+setupWebsocket(server);
+
+mongoose.connect(
+    'mongodb+srv://flexaBR:j0c@2014@cluster0-tdoao.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+app.use(cors())
+app.use(express.json());
+app.use(routes);
+
+server.listen(3333);
